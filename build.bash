4587d0e229b17be18ead00989ef19f8de292001d
yyNoBug <yaoyuan2000@sjtu.edu.cn>
2020-04-05 20:38:50 +0800

    Change bash.

--- build.bash
+++ build.bash
@@ -2,4 +2,4 @@ set -e
 cd "$(dirname "$0")"
 echo "$0"
 mkdir -p bin
-find ./src -name *.java | javac -d bin -classpath "ulib/java/antlr-4.8-complete.jar" @/dev/stdin
+find ./src -name *.java | javac -d bin -classpath "/ulib/java/antlr-4.8-complete.jar" @/dev/stdin
